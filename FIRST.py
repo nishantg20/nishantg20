f967acbfb88878ed515ff97e8c275607c0f18756
Nishant Gupta <nigupta639@gmail.com>
2024-03-18 15:00:38 +0530

    Add files via upload

---
+++ FIRST.py
@@ -0,0 +1,9 @@
+a=int(input('enter first(a) no.'))
+b=int(input('enter second(b) no.'))
+c=int(input('enter third(c) no.'))
+if a>b and a>c:
+    print('a is greater')
+elif b>a and b>c:
+    print('b is greater')
+else:
+    print('c is greater')
